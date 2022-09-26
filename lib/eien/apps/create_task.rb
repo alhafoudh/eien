7d7518d3f3fc1721e5b8f8a579f12cac39f961d6
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2022-09-26 12:53:17 +0200

    Create also namespace for app if it does not exist

--- lib/eien/apps/create_task.rb
+++ lib/eien/apps/create_task.rb
@@ -14,9 +14,15 @@ module Eien
       end
 
       def run!
-        client = kubeclient_builder.build_eien_kubeclient(context)
+        create_app!
+        ensure_namespace!
+      end
 
-        resource = Kubeclient::Resource.new(
+      private
+
+      def create_app!
+        client = kubeclient_builder.build_eien_kubeclient(context)
+        app_resource = Kubeclient::Resource.new(
           metadata: {
             name: name
           },
@@ -24,7 +30,25 @@ module Eien
             namespace: spec_namespace
           }
         )
-        client.create_app(resource)
+        client.create_app(app_resource)
+      end
+
+      def ensure_namespace!
+        client = kubeclient_builder.build_v1_kubeclient(context)
+        begin
+          client.get_namespace(spec_namespace)
+        rescue Kubeclient::ResourceNotFoundError
+          create_namespace!(client)
+        end
+      end
+
+      def create_namespace!(client)
+        namespace_resource = Kubeclient::Resource.new(
+          metadata: {
+            name: spec_namespace
+          }
+        )
+        client.create_namespace(namespace_resource)
       end
     end
   end
